f58c0a64967aa0817f9c0544e453d3eff82b255c
TzaddiLamed <tzaddi.lamed@gmail.com>
2024-10-29 08:15:03 -0500

    Made some modifications to the listing container and the footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,28 +7,31 @@ const Footer = () => {
     <>
       <footer>
         <Container fluid>
-            <Row className='bg-dark text-white pt-3'>
+            <Row className='bg-dark text-white pt-4'>
                 <Col sm={12} md={4} className="mx-5">
                     <Stack>
-                        <h2>Wiliams Realty</h2>
-                        <p>Tagline (tbd)</p>
+                        <h2 className='footer-brand'>Wiliams Realty</h2>
+                        <p>Let us help you find your dream home.</p>
                     </Stack>
                 </Col>
                 <Col sm={12} md={4}>
                     <Nav className='flex-column fs-5'>
-                        <h3 className='ms-3'>Useful Links</h3>
-                        <NavLink href='#' className='text-white fs-6'>Home</NavLink>
-                        <NavLink href='#' className='text-white fs-6'>About</NavLink>
-                        <NavLink href='#' className='text-white fs-6'>Listings</NavLink>
-                        <NavLink href='#' className='text-white fs-6'>Contact</NavLink>
-                        <NavLink href='#' className='text-white fs-6'>Contact</NavLink>
+                        <h4 className='ms-3'>Useful Links</h4>
+                        <NavLink href='#' className='text-white fs-6 foot-nav-item'>Home</NavLink>
+                        <NavLink href='#' className='text-white fs-6 foot-nav-item'>About</NavLink>
+                        <NavLink href='#' className='text-white fs-6 foot-nav-item'>Listings</NavLink>
+                        <NavLink href='#' className='text-white fs-6 foot-nav-item'>Contact</NavLink>
+                        <NavLink href='#' className='text-white fs-6 foot-nav-item'>Contact</NavLink>
                     </Nav>
                 </Col>
                 <Col sm={12} md={3}>
-                    <h3>Contact us:</h3>
+                    <h4>Contact us:</h4>
                     <p>info@williamsrealty.com</p>
                     <p>(888)123-4567</p>
                 </Col>
+            </Row>
+            <Row className='bg-dark text-white pt-3 pb-2'>
+                <p className="copy-info">&copy; 2021 Williams Realty. All rights reserved</p>
             </Row>
         </Container>
       </footer>
